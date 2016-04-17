22103254339eed9f1ba2d2dfb99d4f40612d80d0
mjxlnir <waderentz@gmail.com>
2016-04-16 22:07:03 -0400

    oopsy css change

--- admin/admin_functions/message/message_functions.js
+++ admin/admin_functions/message/message_functions.js
@@ -11,13 +11,13 @@ $(document).ready(function() {
 				$('#message').html('<h2>Message Inbox</h2><p>Check your messages below.</p><div class="error"><p>Inbox empty!</p></div>');
 			} else {
 	// Draw table with messages from database
-			$("#message_table").append('<tr><th>#</th><th>From:</th><th>Subject:</th><th></th></tr>');
-			$.each(data, function(index,val){
-				$("#message_table").append('<tr id="row" class="'+data[index]['id']+'"><td>'+(index+1)+'</td><td><a class="inbox" id="'+index+'"href="">'+data[index]['name']+'</a></td><td>'+data[index]['subject']+'</td><td><span id="'+data[index]['id']+'"><button>Delete</button></span></td></tr>');
-			});
-			remove(data);
-			click(data);
-		}
+				$("#message_table").append('<tr><th>#</th><th>From:</th><th>Subject:</th><th></th></tr>');
+				$.each(data, function(index,val){
+					$("#message_table").append('<tr id="row" class="'+data[index]['id']+'"><td>'+(index+1)+'</td><td><a class="inbox" id="'+index+'"href="">'+data[index]['name']+'</a></td><td>'+data[index]['subject']+'</td><td><span id="'+data[index]['id']+'"><button>Delete</button></span></td></tr>');
+				});
+				remove(data);
+				click(data);
+			}
 		}
 	});
 
@@ -31,7 +31,7 @@ $(document).ready(function() {
 			var subject = data[id]['subject'];
 			var message = data[id]['message'];
 			$('#message').html('<h4><b>From:</b> '+name+'</h4><h4><b>Email:</b> '+email+'</h4><h4><b>Subject:</b> '+subject+'</h4><hr /><br /><p>'+message+'</p><br /><br /><button type="button" class="btn">Return to Inbox</button>');
-			});
+		});
 			inbox();
 	}
 	
@@ -55,7 +55,6 @@ $(document).ready(function() {
 					e.preventDefault();
 					$('.error').append('<p>Message deleted! Reloading...</p>');
 					$('tr').remove();
-					//alert(("."+id));
 					setTimeout(function(){
 						location.reload();
 					}, 1300);
