e317b86dead6de23074c63c7374d569e55f03f16
mjxlnir <waderentz@gmail.com>
2016-04-16 22:20:34 -0400

    code formatting

--- about.php
+++ about.php
@@ -1,25 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>About Ordino</title>
- <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/local.css">
-  <link rel="stylesheet" href="css/bootstrap.css">
- 
-  <link href='http://fonts.googleapis.com/css?family=Quicksand:400,300,700' rel='stylesheet' type='text/css'>
+	<title>About Ordino</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/local.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link href='http://fonts.googleapis.com/css?family=Quicksand:400,300,700' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open Sans:400,300,700' rel='stylesheet' type='text/css'>
 	<link href="https://cdn.datatables.net/1.10.10/css/dataTables.bootstrap.min.css" rel="stylesheet" type='text/css'>
-
-  <style>
-  </style>
 </head>
 <body >
-
-  <!-- Navigation -->
+<!-- Navigation -->
     <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
         <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
+<!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle toggle-button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -27,11 +22,11 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-					<a class="navbar-brand" href="#">
+					<a class="navbar-brand" href="index.html">
 					<h1 id="ordino">Ordino</h1>
 					</a>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
+<!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse nav-right" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
@@ -48,56 +43,59 @@
                     </li>
                 </ul>
             </div>
-            <!-- /.navbar-collapse -->
+<!-- /.navbar-collapse -->
         </div>
-        <!-- /.container -->
+<!-- /.container -->
     </nav>
-
     <div class="container" style="margin-top: 120px;">
-  <div class="row">
-    <div class="col-md-12">
-    <h2 class="bold">How It Started</h2>
-    <p>Ordino is the culmination of our group's desire to develop a compact and agile system to solve a particular need within Georgia Southern's IT Department. We noticed a particular problem with the traceability of student accessible hardware in the IT labs. Thus, the idea for Ordino was born. 
-</p><br>
-    <h2 class="bold">How It Works</h2>
-    <p>To use Ordino, simply select a hardware item from the tables, fill out the form on the product page, and submit the request. It’s that simple. To return, simply select the hardware which you previously checked out from the Returns page, fill out the form, and submit the return request. 
-</p><br />
-</div>
-</div>
-<h2 class="bold">Contact Us</h2><br>
-<form class="form-horizontal" method="post" action="splash.php">
-  <div class="form-group">
-    <label for="name" class="col-sm-1 control-label text-left">Name</label>
-    <div class="col-sm-11">
-      <input type="text" class="form-control" name="name" id="name" placeholder="Name">
-    </div>
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="bold">How It Started</h2>
+					<p>
+					Ordino is the culmination of our group's desire to develop a compact and agile system to solve a particular need within Georgia Southern's IT Department. We noticed a particular problem with the traceability of student accessible hardware in the IT labs. Thus, the idea for Ordino was born. 
+					</p>
+					<br />
+				<h2 class="bold">How It Works</h2>
+					<p>
+					To use Ordino, simply select a hardware item from the tables, fill out the form on the product page, and submit the request. It’s that simple. To return, simply select the hardware which you previously checked out from the Returns page, fill out the form, and submit the return request. 
+					</p>
+					<br />
+			</div>
+		</div>
+		<h2 class="bold">Contact Us</h2>
+		<br />
+		<form class="form-horizontal" method="post" action="splash.php">
+			<div class="form-group">
+				<label for="name" class="col-sm-1 control-label text-left">Name</label>
+				<div class="col-sm-11">
+					<input type="text" class="form-control" name="name" id="name" placeholder="Name">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="name" class="col-sm-1 control-label text-left">E-Mail</label>
+				<div class="col-sm-11">
+					<input type="email" class="form-control" name="email" id="email" placeholder="E-mail">
+				</div>
+			</div>
+			<div class="form-group">
+			<label for="subject" class="col-sm-1 control-label text-left">Subject</label>
+				<div class="col-sm-11">
+					<input type="text" class="form-control" name="subject" id="subject" placeholder="Subject">
+				</div>
+			</div>
+			<div class="form-group">
+			<label for="message" class="col-sm-1 control-label text-left">Message</label>
+				<div class="col-sm-11">
+					<textarea class="form-control" rows="5" name="message" id="message" placeholder="What's on your mind? (500 Character Limit)"></textarea>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-xs-12">
+					<input type="submit" class="btn btn-default pull-right" id="message_submit" name="Submit" value="Submit"> 
+				</div>
+			</div>
+		</form>
 	</div>
-	<div class="form-group">
-	 <label for="name" class="col-sm-1 control-label text-left">E-Mail</label>
-    <div class="col-sm-11">
-      <input type="email" class="form-control" name="email" id="email" placeholder="E-mail">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="subject" class="col-sm-1 control-label text-left">Subject</label>
-    <div class="col-sm-11">
-      <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject">
-    </div>
-  </div>
-  <div class="form-group">
-  <label for="message" class="col-sm-1 control-label text-left">Message</label>
-  <div class="col-sm-11">
-  <textarea class="form-control" rows="5" name="message" id="message" placeholder="What's on your mind? (500 Character Limit)"></textarea>
-  </div>
-  </div>
-
-  <div class="form-group">
-    <div class="col-xs-12">
-      <input type="submit" class="btn btn-default pull-right" id="message_submit" name="Submit" value="Submit"> 
-    </div>
-  </div>
-</form>
-</div>
 <!--scripts -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
