2948b95813d8d205cd96f5eced5e26123a94bb90
mjxlnir <waderentz@gmail.com>
2016-04-19 21:16:38 -0400

    More admin MVC

--- admin/views/register.php
+++ admin/views/register.php
@@ -1,31 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-	<link href='http://fonts.googleapis.com/css?family=Quicksand:400,300,700' rel='stylesheet' type='text/css'>
-	<link rel="icon" type="image/png" href="../css/favicon-32x32.png" sizes="32x32">
-    <title>Ordino - Admin Registration</title>
-<!-- Bootstrap Core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-<!-- Custom CSS -->
-    <link href="../css/simple-sidebar.css" rel="stylesheet">
-
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/admin/admin_template/admin_head.php'); ?>
+	<title>Ordino - Admin Registration</title>
 </head>
 <body>
     <div id="wrapper">
 <!-- Sidebar -->
        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/admin/admin_template/admin_nav.php'); ?>
-<!-- /#sidebar-wrapper -->
 <!-- nav bar at top -->
 		<nav class="navbar navbar-default navbar-static-top">
 			<div id="page" class="container">
@@ -40,18 +22,18 @@
 		<div class="error">
 		<?php
 		// show potential errors / feedback (from registration object)
-		if (isset($registration)) {
+			if (isset($registration)) {
     			if ($registration->errors) {
         			foreach ($registration->errors as $error) {
             				echo $error;
        			 	}
-   		 	}
+				}
     			if ($registration->messages) {
         			foreach ($registration->messages as $message) {
             				echo $message;
         			}
     			}
-		}
+			}
 		?>
 		</div>
 		<br />
@@ -91,9 +73,6 @@
 <!-- /#page-content-wrapper -->
     </div>
 <!-- /#wrapper -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>	
-	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="admin_functions/admin_functions.js"></script>
+	<?php require_once($_SERVER['DOCUMENT_ROOT'].'/admin/admin_template/admin_script.php'); ?>
 </body>
 </html>
