acbc20fc00100e0fe8e97f767a8316b9885a03bb
mjxlnir <waderentz@gmail.com>
2016-04-20 19:52:11 -0400

    code format

--- admin/admin_functions/admin_functions.js
+++ admin/admin_functions/admin_functions.js
@@ -53,8 +53,9 @@ $(document).ready(function() {
 						}
 					});
 				}
-			break;
+				break;
 			case 'Edit Hardware':
+			
 				var id = $('#id').val();
 				var type = $('#type').val();
 				var model = $('#model').val();
@@ -77,75 +78,74 @@ $(document).ready(function() {
 						}
 					});
 				}
-			break;
+				break;
 			case 'Delete Hardware':
-			var id = $('#id').val();
-			if (!id){
-				$('.error').html('<p>You must enter an ID!</p>');
-				$('#hardware_form')[0].reset();
-			} else {
-				var dataString = {function_switch: function_switch,id: id};
-				$.ajax({
-					type: 'POST',
-					url: 'admin_functions/admin_functions.php',
-					data: dataString,
-					success: function(data){
-						$('.error').html('<p>Hardware has been deleted from the database!</p>');
-						$('#hardware_form')[0].reset();
-						/*setTimeout(function(){
-							window.location.reload();
-						}, 1500); */
-					}
-				});
-			}
-			break;
-		case 'Edit Account':
-			var user = $('#username').val();
-			var email = $('#email').val();
-			var password = $('#password').val();
 			
-			if (!user || !email || !password){
-				$('.error').html('<p>You must fill out all form sections!</p>');
-				$('#account_form')[0].reset();
-			}else {
-				var dataString = {function_switch: function_switch, user: user, email:email, password:password};
-				$.ajax({
-					type: 'POST',
-					url: 'admin_functions/admin_functions.php',
-					data: dataString,
-					success: function(data){
-						alert(data);
-						$('.error').html('<p>The admin account: <b>'+user+'</b> has been edited in the database!</p>');
-						$('#account_form')[0].reset();
-					}
-				});
-			}
-			break
-		case 'Delete Account':
+				var id = $('#id').val();
+				if (!id){
+					$('.error').html('<p>You must enter an ID!</p>');
+					$('#hardware_form')[0].reset();
+				} else {
+					var dataString = {function_switch: function_switch,id: id};
+					$.ajax({
+						type: 'POST',
+						url: 'admin_functions/admin_functions.php',
+						data: dataString,
+						success: function(data){
+							$('.error').html('<p>Hardware has been deleted from the database!</p>');
+							$('#hardware_form')[0].reset();
+						}
+					});
+				}
+				break;
+			case 'Edit Account':
 		
-			var user = $('#username').val();
-			var email = $('#email').val();
-			var password = $('#password').val();
+				var user = $('#username').val();
+				var email = $('#email').val();
+				var password = $('#password').val();
 			
-			if (!user || !email || !password){
-				$('.error').html('<p>You must fill out all form sections!</p>');
-				$('#account_form')[0].reset();
-			}else {
-				var dataString = {function_switch: function_switch, user: user, email:email, password:password};
-				$.ajax({
-					type: 'POST',
-					url: 'admin_functions/admin_functions.php',
-					data: dataString,
-					success: function(data){
-						alert(data);
-						$('.error').html('<p>The account: <b>' +user+'</b> has been deleted from the database!</p>');
-						$('#account_form')[0].reset();
-						setTimeout(function(){
-							window.location.reload();
-						}, 1500); 
-					}
-				});
-			}
+				if (!user || !email || !password){
+					$('.error').html('<p>You must fill out all form sections!</p>');
+					$('#account_form')[0].reset();
+				}else {
+					var dataString = {function_switch: function_switch, user: user, email:email, password:password};
+					$.ajax({
+						type: 'POST',
+						url: 'admin_functions/admin_functions.php',
+						data: dataString,
+						success: function(data){
+							alert(data);
+							$('.error').html('<p>The admin account: <b>'+user+'</b> has been edited in the database!</p>');
+							$('#account_form')[0].reset();
+						}
+					});
+				}
+				break
+			case 'Delete Account':
+		
+				var user = $('#username').val();
+				var email = $('#email').val();
+				var password = $('#password').val();
+			
+				if (!user || !email || !password){
+					$('.error').html('<p>You must fill out all form sections!</p>');
+					$('#account_form')[0].reset();
+				}else {
+					var dataString = {function_switch: function_switch, user: user, email:email, password:password};
+					$.ajax({
+						type: 'POST',
+						url: 'admin_functions/admin_functions.php',
+						data: dataString,
+						success: function(data){
+							alert(data);
+							$('.error').html('<p>The account: <b>' +user+'</b> has been deleted from the database!</p>');
+							$('#account_form')[0].reset();
+						}
+					});
+				}
+				break;
+			default :
+			
 			break;
 		}
 	});
