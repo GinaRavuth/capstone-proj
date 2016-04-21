a3eeff6a930b6bedf877715638e99aad3eb1c887
mjxlnir <waderentz@gmail.com>
2016-04-21 04:09:35 -0400

    favicon test

--- admin/admin_template/admin_head.php
+++ admin/admin_template/admin_head.php
@@ -11,6 +11,28 @@
 <link href="../css/bootstrap.min.css" rel="stylesheet">
 <!-- Custom CSS -->
 <link href="../css/simple-sidebar.css" rel="stylesheet">
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="57x57" href="../css/favicon/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="../css/favicon/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="../css/favicon/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="../css/favicon/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="../css/favicon/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="../css/favicon/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="../css/favicon/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="../css/favicon/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="../css/favicon/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="../css/favicon/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="../css/favicon/android-chrome-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="../css/favicon/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="../css/favicon/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="/manifest.json">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="apple-mobile-web-app-title" content="Ordino">
+<meta name="application-name" content="Ordino">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<!-- End Favicon -->
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
