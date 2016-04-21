f5e5f31bf5d33e4655ab5be0774a652f2b85e67b
Alex Goff <alexander.r.goff@gmail.com>
2016-04-21 15:09:06 -0400

    checkouts and returns sending to back end

--- splash.php
+++ splash.php
@@ -13,21 +13,6 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/include/functions.php');
 	$url = $return['url'];
 	$text = $return['text'];
 		
-} else if(isset($_POST['Checkout'])) {
-	
-	if (!empty($_POST['name']) && !empty($_POST['eagleid']) && !empty($_POST['message'])){
-		$return = splash_text('Checkout');
-		$display = $return['display'];
-		$status = $return['status'];
-		$url = $return['url'];
-		$text = $return['text'];
-	} else {
-		$return = splash_text('Empty');
-		$display = $return['display'];
-		$status = $return['status'];
-		$url = $return['url'];
-		$text = $return['text'];
-	}
 } else if (isset($_POST['Return'])){
 		
 	$return = splash_text('Return');
@@ -36,6 +21,12 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/include/functions.php');
 	$url = $return['url'];
 	$text = $return['text'];
 	
+} else {
+	$return = splash_text('');
+	$display = $return['display'];
+	$status = $return['status'];
+	$url = $return['url'];
+	$text = $return['text'];	
 }
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/splash_template.php');
 ?>
