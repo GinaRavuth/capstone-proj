babcfef126caf8e4a85e802d13a84c9af07ad747
mjxlnir <waderentz@gmail.com>
2016-04-20 23:07:03 -0400

    some responsive module styling

--- admin/views/account/manage_accounts.php
+++ admin/views/account/manage_accounts.php
@@ -15,11 +15,67 @@
 		</nav>
 <!-- End top navbar -->
 <!-- Page Content -->
-		<div class="col-sm-12">
+		<div id="content_header" class="col-sm-6">
 			<h2 >Manage Accounts</h2>
-				<p>Use this module to manage admin accounts. You may add, edit, or delete accounts.</p>
+			<p>Use this module to manage admin accounts. You may add, edit, or delete accounts.</p>
 			<br />
-			<br />
+		</div>
+		 <div id="manage_hardware">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-4 col-sm-6 spaced">
+					<a href="index.php?view=create_account">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div><br/><br/>
+									<div class="col-lg-12 text-center">
+										<h2>Add Account</h2>
+										<span class="glyphicon glyphicon-plus"></span>
+									</div>
+                                </div>
+                            </div>
+                        </div>
+					</a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 spaced">
+					<a href="index.php?view=edit_account">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div><br/><br/>
+									<div class="col-lg-12 text-center">
+										<h2>Edit Account</h2>
+										<span class="glyphicon glyphicon-edit"></span>					
+									</div>
+                                </div>
+                            </div>
+                        </div>
+					</a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 spaced">
+					<a href="index.php?view=delete_account">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div><br/><br/>
+									<div class="col-lg-12 text-center">
+										<h2>Delete Account</h2>
+										<span class="glyphicon glyphicon-trash"></span>										
+									</div>
+                                </div>
+                            </div>
+                        </div>
+					</a>
+                    </div>
+				</div>
+			</div>
 		</div>
 <!-- End page content -->
     </div>
--- admin/views/hardware/manage_hardware.php
+++ admin/views/hardware/manage_hardware.php
@@ -8,19 +8,76 @@
     <div id="wrapper">
 		<?php require_once($_SERVER['DOCUMENT_ROOT'].'/admin/admin_template/admin_nav.php'); ?>
 <!-- Admin dashboard top navbar -->
-		<nav class="navbar navbar-default navbar-static-top">
-		  <div id="page" class="container">
+		<nav class="navbar navbar-default navbar-static-top module">
+			<div id="page" class="container text-center">
 			<h1>Ordino</h1>
 		  </div>
 		</nav>
+
 <!-- End top navbar -->
 <!-- Page Content -->
-		<div class="col-sm-12">
-			<h2 >Manage Hardware</h2>
-				<p>Use this module to manage your hardware. You may add, edit, or delete hardware.</p>
-			<br />
+		<div id="content_header" class="col-sm-6">
+			<h2>Manage Hardware</h2>
+			<p>Use this module to manage hardware. You may add, edit, or delete hardware</p>
 			<br />
 		</div>
+		 <div id="manage_hardware">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-4 col-sm-6 spaced">
+					<a href="index.php?view=add">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div><br/><br/>
+									<div class="col-lg-12 text-center">
+										<h2>Add Hardware</h2>
+										<span class="glyphicon glyphicon-plus"></span>
+									</div>
+                                </div>
+                            </div>
+                        </div>
+					</a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 spaced">
+					<a href="index.php?view=edit">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div><br/><br/>
+									<div class="col-lg-12 text-center">
+										<h2>Edit Hardware</h2>
+										<span class="glyphicon glyphicon-edit"></span>					
+									</div>
+                                </div>
+                            </div>
+                        </div>
+					</a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 spaced">
+					<a href="index.php?view=delete">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div><br/><br/>
+									<div class="col-lg-12 text-center">
+										<h2>Delete Hardware</h2>
+										<span class="glyphicon glyphicon-trash"></span>										
+									</div>
+                                </div>
+                            </div>
+                        </div>
+					</a>
+                    </div>
+				</div>
+			</div>
+		</div>		
 <!-- End page content -->
     </div>
 <!-- End wrapper -->
